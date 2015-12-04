e2427a6ec8cc3b99f625b9809baa9e86375b0e9f
root <root@localhost.localdomain>
2015-12-04 05:03:29 +0100

    api

--- api/api.php
+++ api/api.php
@@ -97,7 +97,7 @@
     {
         try
         {
-            $bdd = new PDO('mysql:host=localhost;dbname=allert;charset=utf8', 'root', '');
+            $bdd = new PDO('mysql:host=localhost;dbname=allert;charset=utf8', 'root', 'mysql');
         }
         catch(Exception $e)
         {
